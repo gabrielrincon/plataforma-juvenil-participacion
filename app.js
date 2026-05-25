ac051017fbe27eb17901e167aee644ef13f0a169
Gabriel Rincón <anivelmundial@hotmail.com>
2026-05-25 03:44:54 +0000

    Clase 28 pantalla de candidatos

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const btnMensaje = document.getElementById('btnMensaje')
 const mensajeClase = document.getElementById('mensajeClase')
+const botonesPerfil = document.querySelectorAll('.btnPerfil')
+const detallePerfil = document.getElementById('detallePerfil')
 
 let contadorDeClicks = 0
 
@@ -9,4 +11,11 @@ btnMensaje.addEventListener('click', () => {
     contadorDeClicks = contadorDeClicks + 1
 
 
+})
+
+botonesPerfil.forEach((boton) => {
+    boton.addEventListener('click', () => {
+        const perfil = boton.getAttribute('data-perfil')
+        detallePerfil.textContent = `Información detallada del perfil: ${perfil}`
+    })
 })
